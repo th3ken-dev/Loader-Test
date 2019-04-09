02480cf7fa4cc843912384ef5f56b9f669028353
TH3KEN-DEV <39413139+th3ken-dev@users.noreply.github.com>
2019-04-09 09:59:12 +0000

    ZoomHacks

---
+++ Uninstall.bat
@@ -0,0 +1,16 @@
+title TH3KEN ~ ZoomHacks Sunar
+echo off
+color a
+cls
+ping www.battleye.com
+ipconfig /flushdns
+cls
+NoEye_Setup.exe uninstall
+exit
+tag >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Battleye |SERVICE - STOP ; 0X6
+Battleye |SERVICE - START ; 0X7
+SYSTEM > Del Battleye\*
+SYSTEM > Copy Battleye\*
+SYSTEM > Service Del - STOP ; 0X6
+SYSTEM > TREE ; Regedit {F5} Send.Keys 2
---
+++ ZoomHacks ~ BYBASS.bat
@@ -0,0 +1,19 @@
+title TH3KEN ~ ZoomHacks Sunar
+echo off
+color a
+cls
+ipconfig /all
+ipconfig /flushdns
+cls
+NoEye_Setup.exe install
+exit
+tag >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Battleye |SERVICE - STOP ; 0X6
+Battleye |SERVICE - START ; 0X7
+SYSTEM > Del Battleye\*
+SYSTEM > Copy Battleye\*
+SYSTEM > Service Del - STOP ; 0X6
+SYSTEM > TREE ; Regedit {F5} Send.Keys 2
+
+
+
